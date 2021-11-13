6ecfe5c8c775ee91d1756aff9798a95685ab39a0
Rohan2596 <rohankadam662@gmail.com>
2021-11-13 15:44:43 +0530

    ADDED:Version Proxy Pattern for Delivery Pattern.

--- src/com/designPattern/DesignPattern.java
+++ src/com/designPattern/DesignPattern.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 /*
  * @author : ROHAN RAVINDRA KADAM
- * @purpose: Proxy  Design pattern main class
+ * @purpose: Facade  Design pattern main class
  * @name   : DesignPattern
  * */
 public class DesignPattern {
@@ -15,17 +15,15 @@ public class DesignPattern {
 
         DeliveryManager deliveryManager = new DeliveryManager();
 
-        //Package Info Size Small and weight 5kgs
-        PackageInfo packageInfoSmall = new PackageInfo("SMALL", 5);
-        System.out.println(deliveryManager.deliveryManagerFactory(packageInfoSmall));
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the Package Size SMALL,LARGE OR MEDIUM");
+       String size=scanner.next();
 
-        //Package Info Size Medium and weight 12kgs
-        PackageInfo packageInfoMedium = new PackageInfo("MEDIUM", 12);
-        System.out.println(deliveryManager.deliveryManagerFactory(packageInfoMedium));
+        System.out.println("Enter the Package weight ie 5,10 or 20");
+        int weight=scanner.nextInt();
 
-        //Package Info Size Large and weight 20kgs
-        PackageInfo packageInfoLarge = new PackageInfo("LARGE", 52);
-        System.out.println(deliveryManager.deliveryManagerFactory(packageInfoLarge));
+        PackageInfo packageInfo= new PackageInfo(size,weight);
+        System.out.println(deliveryManager.deliveryManagerFactory(packageInfo));
 
     }
 }
